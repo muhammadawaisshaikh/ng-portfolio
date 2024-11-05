c14abbc95558d17848a49f84678a5c1fc8185cdf
Muhammad Awais <awaisshaikh94@gmail.com>
2024-11-05 16:43:24 +0500

    create a horizontal scrolling effect for the background image

---
+++ src/app/components/recognitions-contributions/recognitions-contributions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recognitions-contributions',
+  standalone: true,
+  imports: [],
+  templateUrl: './recognitions-contributions.component.html',
+  styleUrl: './recognitions-contributions.component.scss'
+})
+export class RecognitionsContributionsComponent {
+
+}
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { RecognitionsContributionsComponent } from '../../components/recognitions-contributions/recognitions-contributions.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, RecognitionsContributionsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
