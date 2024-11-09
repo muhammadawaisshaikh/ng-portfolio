bfd7d98163f1ba7dc64d0ed2e3165e6fb1b796ce
Muhammad Awais <awaisshaikh94@gmail.com>
2024-11-09 18:20:28 +0500

    setup auxilary routes with a new outlet to change router for wow like page switching above the canvas (#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { LoadingSplashComponent } from './components/loading-splash/loading-splash.component';
 import { GlobalStateService } from './services/global-state/global-state.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoadingSplashComponent],
+  imports: [NgClass, RouterOutlet, LoadingSplashComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   isHide: boolean = false;
+  isPageAnimate: boolean = false;
   
   constructor(
-    private globalState: GlobalStateService
+    private globalState: GlobalStateService,
+    private router: Router
   ) { }
   
   title = 'ng-portfolio';
@@ -22,6 +25,15 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.globalState.isHide.subscribe((val: boolean) => {
       this.isHide = val;
+    });
+
+    this.globalState.isPageAnimate.subscribe((val: boolean) => {
+      this.isPageAnimate = val;
     })
+  }
+
+  close() {
+    this.globalState.setIsPageAnimate(false);
+    this.router.navigate(['']);
   }
 }
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { GlobalStateService } from '../../services/global-state/global-state.service';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +11,21 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
   tabs: { path: string, name: string }[] = [
-    { path: '/experience', name: 'Experience'},
-    { path: '/portfolio', name: 'Portfolio'},
-    { path: '/technology-talks', name: 'Technology Talks'},
-    { path: '/certifications', name: 'Certifications'},
-    { path: '/publications', name: 'Publications'},
-    { path: '/honors-awards', name: 'Honors & Awards'},
-  ]
+    { path: 'experience', name: 'Experience'},
+    { path: 'portfolio', name: 'Portfolio'},
+    { path: 'technology-talks', name: 'Technology Talks'},
+    { path: 'certifications', name: 'Certifications'},
+    { path: 'publications', name: 'Publications'},
+    { path: 'honors-awards', name: 'Honors & Awards'},
+  ];
+
+  constructor(
+    private globalState: GlobalStateService,
+    private router: Router
+  ) { }
+
+  onNavigate(link: string) {
+    this.globalState.setIsPageAnimate(true);
+    this.router.navigate([{ outlets: { pages: [link] } }]);
+  }
 }
