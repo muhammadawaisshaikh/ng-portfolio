03191a40dd9acd931483f52f77ae8d13b0683611
Muhammad Awais <awaisshaikh94@gmail.com>
2024-11-11 18:07:18 +0500

    sccss renamed

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api/api.service';
+import { IAbout } from '../../interfaces/about';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
   imports: [],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent implements OnInit {
 
@@ -19,7 +20,7 @@ export class ExperienceComponent implements OnInit {
   }
 
   getAboutData() {
-    this.apiService.getAboutInfo().subscribe((res: any) => {
+    this.apiService.getAboutInfo().subscribe((res: IAbout) => {
       console.log('About', res);
     });
   }
