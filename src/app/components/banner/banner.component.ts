ec8e399456bf6f46d646c8ccc2e53a07ab75493e
Muhammad Awais <awaisshaikh94@gmail.com>
2024-12-17 16:35:11 +0500

    hotfix

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -22,7 +22,7 @@ export class BannerComponent implements OnInit, OnDestroy {
       height: '50px'
     },
     content: {
-      width: '1080px',
+      width: '100%',
       height: '50px',
     },
     position: 'horizontal'
