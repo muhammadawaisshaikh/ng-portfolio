45e304600cb9471da94eb3cae27029f051ef193a
Muhammad Awais <awaisshaikh94@gmail.com>
2024-12-17 16:01:35 +0500

    fixes

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
-import { RecognitionsContributionsComponent } from '../../components/recognitions-contributions/recognitions-contributions.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [BannerComponent, RecognitionsContributionsComponent],
+  imports: [BannerComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
